6e2f18ca330595cd85be9c51b61c20ed07acf37f
bitmotte <bitmotte@proton.me>
2026-01-07 08:11:20 -0500

    Trying again with mapping objects. . .

--- AssetLoader.cs
+++ AssetLoader.cs
@@ -7,11 +7,13 @@ namespace LessScaryMannequins;
 public static class AssetLoader
 {
     public static GameObject CopyTemplate;
+    public static RuntimeAnimatorController CoolController;
 
     public static void Load()
     {   
         AssetBundle bundle = AssetBundle.LoadFromFile(Path.Combine(Paths.PluginPath, "LessScaryMannequins/resources/copy.bundle"));
         CopyTemplate = (GameObject)bundle.LoadAsset("Assets/Copy.prefab");
+        CoolController = (RuntimeAnimatorController)bundle.LoadAsset("Assets/CoolController.controller");
         bundle.Unload(false);
     }
 }
--- ChangeMesh.cs
+++ ChangeMesh.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+using HarmonyLib;
 using UnityEngine;
 
 namespace LessScaryMannequins;
@@ -6,23 +8,39 @@ public static class ChangeMesh
 {
     public static void Change(GameObject mannequin)
     {
-        Object.Destroy(mannequin.transform.GetChild(0).gameObject);
+        Plugin.Logger.LogInfo("CHANGING MANNEQUIN : " + mannequin.name);
 
-        GameObject copy = Object.Instantiate(AssetLoader.CopyTemplate, mannequin.transform);
-        GameObject copyMesh = copy.transform.GetChild(0).gameObject;
-        copyMesh.transform.parent = mannequin.transform;
-        copyMesh.transform.localScale = new Vector3(3.2f,3.2f,3.2f);
+        //new objects to paste
+        GameObject birdie = Object.Instantiate(AssetLoader.CopyTemplate).transform.GetChild(0).gameObject;
+        GameObject birdieArmature = birdie.transform.GetChild(0).gameObject;
+        
+        //old object to pull data
+        GameObject quinn = mannequin.transform.GetChild(0).gameObject;
+        GameObject quinnArmature = quinn.transform.GetChild(0).gameObject;
 
-        Animator copyController = copy.GetComponent<Animator>();
-        Animator mannequinController = mannequin.GetComponent<Animator>();
-        mannequinController.avatar = copyController.avatar;
-        mannequinController.runtimeAnimatorController = copyController.runtimeAnimatorController;
 
-        Object.Destroy(copy);
+        Transform[] MappableBones = [];
+        GameObject[] UnmappableBones = [];
+        GameObject[] UnmappableParents = [];
+        for (int i = 0; i < quinnArmature.transform.childCount; i++)
+        {
+            Transform bone = quinnArmature.transform.GetChild(i);
+            if(birdieArmature.transform.Find(bone.name) == true)
+            {
+                Plugin.Logger.LogInfo("Mappable : " + bone.name);
+                MappableBones.AddItem(bone.transform);
+            }
+            else
+            {
+                Plugin.Logger.LogInfo("Unmappable : " + bone.name);
+                UnmappableBones.AddItem(bone.gameObject);
+                UnmappableParents.AddItem(bone.parent.gameObject);
+            }
+        }
     }
 
     public static void DestroyQuinn(GameObject mannequin)
     {
-        GameObject.Destroy(mannequin.transform.GetChild(0).GetChild(1).GetChild(0).gameObject);
+        GameObject.Destroy(mannequin.transform.GetChild(1).gameObject);
     }
 }
